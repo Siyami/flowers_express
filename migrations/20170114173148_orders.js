206d07d3b3055a228d3499c0f8577714bbb874ec
PaulMill <usa.paul@icloud.com>
2017-01-15 19:47:48 -0800

    Create orders, carts, recepients migration files

--- migrations/20170114173148_orders.js
+++ migrations/20170114173148_orders.js
@@ -1,8 +1,33 @@
+'use strict';
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function(knex) {
+  return knex.schema.createTable('orders', (table) => {
+    table.increments();
+    table.integer('flower_id')
+      .references('flowers.id')
+      .notNullable()
+      .onDelete('CASCADE')
+      .index();
+    table.string('flowerCode').notNullable().defaultTo('');
+    table.string('flowerPrice').notNullable().defaultTo('');
+    table.string('deliveryDate').notNullable().defaultTo('');
+    table.text('cardMessage').defaultTo('');
+    table.string('specialInstruction').notNullable().defaultTo('');
+    table.integer('recipient_id')
+      .references('recipients.id')
+      .notNullable()
+      .onDelete('CASCADE')
+      .index();
+      table.integer('customer_id')
+        .references('customers.id')
+        .notNullable()
+        .onDelete('CASCADE')
+        .index();
+    table.string('orderIP')..notNullable().defaultTo('0.0.0.0');
+    table.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function(knex) {
+  return knex.schema.dropTable('orders');
 };
--- migrations/20170114173206_carts.js
+++ migrations/20170114173206_carts.js
@@ -1,8 +1,23 @@
+'use strict';
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function(knex) {
+  return knex.schema.createTable('carts', (table) => {
+    table.increments();
+    table.integer('flower_id')
+      .references('flowers.id')
+      .notNullable()
+      .onDelete('CASCADE')
+      .index();
+    table.integer('order_id')
+      .references('orders.id')
+      .notNullable()
+      .onDelete('CASCADE')
+      .index();
+    table.string('daleveryDate')..notNullable().defaultTo('');
+    table.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function(knex) {
+  return knex.schema.dropTable('carts');
 };
