19ff92a8087aa7452063ad3c1d2c9afc335c712e
Siyami Avci <siyamiavci@gmail.com>
2017-01-16 09:23:35 -0800

    Fix bugs at routes/customers.js file

--- routes/customers.js
+++ routes/customers.js
@@ -10,8 +10,17 @@ router.post('/customers', (req, res, next) => {
   bcrypt.hash(req.body.password, 12)
     .then((hashed_password) => {
       return knex('customers').insert({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
         email: req.body.email,
-        hashed_password: hashed_password
+        hashed_password: hashed_password,
+        address1: req.body.address1,
+        address2: req.body.address2,
+        city: req.body.city,
+        state: req.body.state,
+        country: req.body.country,
+        phone: req.body.phone,
+        zipcode: req.body.zipcode
       }, '*');
     })
     .then((customers) => {
